e0963a6ab2c2de559d59bb4536a0727cd7405e6f
ssandorov <49904591+SSandorov@users.noreply.github.com>
2021-12-02 17:06:21 +0100

    Creación de la estructura de la SPA (módulos, componentes, ...))

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { PaisesModule } from './paises/paises.module';
+import { SharedModule } from './shared/shared.module';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    PaisesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
